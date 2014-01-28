ccfc920107be34504d977209eb234d3950c78418
Eva Zamudio <gabriela.osu@gmail.com>
2014-01-27 17:33:21 -0800

    update grid class to create rows and raise error if incorrect input

--- grid.rb
+++ grid.rb
@@ -1,7 +1,20 @@
 class Grid
+  attr_reader :string_input
 
-  def initialize
-    
+  def initialize(string_input)
+    create_rows
+    validate(string_input)
+  end
+
+  # create rows of 9 spaces 
+  def create_rows
+    rows = self.string_input.scan(/.{9}/)
+  end
+
+
+  private
+  def validate
+    raise ArgumentError, "Your input must be 81 character long"
   end
 
 end
